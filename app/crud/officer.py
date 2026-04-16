89292842dba7a22b4c4eadca77ed127865c49935
Ben Ashby <100142468+BenAshby04@users.noreply.github.com>
2026-04-16 18:10:05 +0100

    Added update officer

--- app/crud/officer.py
+++ app/crud/officer.py
@@ -7,6 +7,7 @@ from app.schema.officer import Officer
 async def get_officer(db: Session, PID: int):
     return db.query(OfficerModel).filter(OfficerModel.PID == PID).first()
 
+
 async def create_officer(db: Session, officer: Officer):
     new_officer = OfficerModel(**officer.dict())
     
@@ -14,3 +15,15 @@ async def create_officer(db: Session, officer: Officer):
     db.commit()
     db.refresh(new_officer)
     return new_officer
+
+async def update_officer(db: Session, PID: int, officer: Officer):
+    existing_officer = db.query(OfficerModel).filter(OfficerModel.PID == PID).first()
+    if not existing_officer:
+        raise HTTPException(status_code=404, detail="Officer not found")
+
+    for key, value in officer.dict().items():
+        setattr(existing_officer, key, value)
+
+    db.commit()
+    db.refresh(existing_officer)
+    return existing_officer 
