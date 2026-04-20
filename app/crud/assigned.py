55f20e0aadc87276db5b8f0b26e6f3a78895de3c
Ben Ashby <100142468+BenAshby04@users.noreply.github.com>
2026-04-21 00:56:37 +0100

    Added update_assigned function to assigned crud file

--- app/crud/assigned.py
+++ app/crud/assigned.py
@@ -14,3 +14,15 @@ async def create_assigned(db: Session, assigned: Assigned):
     db.commit()
     db.refresh(new_assigned)
     return new_assigned
+
+async def update_assigned(db: Session, DetatchID: int, assigned: Assigned):
+    existing_assigned = db.query(AssignedModel).filter(AssignedModel.DetatchID == DetatchID).first()
+    if not existing_assigned:
+        raise HTTPException(status_code=404, detail="Assigned not found")
+
+    for key, value in assigned.dict().items():
+        setattr(existing_assigned, key, value)
+    db.commit()
+    db.refresh(existing_assigned)
+    return existing_assigned
+
