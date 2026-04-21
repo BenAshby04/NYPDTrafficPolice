ae882497887c901d4382940b9ab49995dca5bbfa
Ben Ashby <100142468+BenAshby04@users.noreply.github.com>
2026-04-21 01:50:18 +0100

    Added User table to DB

--- db_docker/init.sql
+++ db_docker/init.sql
@@ -88,6 +88,12 @@ CREATE TABLE IF NOT EXISTS District(
     DistrictName VARCHAR(50) NOT NULL
 );	
 
+CREATE TABLE IF NOT EXISTS USER(
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(50) NOT NULL,
+    HashedPassword VARCHAR(50) NOT NULL
+);
+
 #Create Relationship Tables
 
 #Creates a table to represent: (Person 1 -> Lives -> N Address)
