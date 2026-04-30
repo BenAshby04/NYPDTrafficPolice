b1c4278b06075cfb7e06b0b5f12e7b2af4d4e333
Ben Ashby <100142468+BenAshby04@users.noreply.github.com>
2026-04-30 16:19:48 +0100

    Added semi colon

--- db_docker/init.sql
+++ db_docker/init.sql
@@ -214,7 +214,7 @@ CREATE TABLE IF NOT EXISTS UserPerson(
     ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY(DLNum) REFERENCES Person(DLNum)
     ON DELETE CASCADE ON UPDATE CASCADE
-)
+);
 
 
 # --------------------------------------
@@ -234,10 +234,11 @@ DELIMITER //
 #Make a procedure to give access to all data relating to a specific driver
 CREATE DEFINER = 'root'@'%'  PROCEDURE GetCiviData(IN dlNum VARCHAR(50)) SQL SECURITY DEFINER
 BEGIN
-	SELECT p.DLNum, p.FName, p.LName, p.DOB, v.VIN, v.LPlate, n.NID, n.Date AS NoticeDate, n.Time AS NoticeTime, n.Location AS NoticeLocation, vc.VioCode, vc.Description AS ViolationDescription, ac.ActCode, ac.Description AS ActionRequired
+	SELECT p.DLNum, p.FName, p.LName, p.DOB, v.VIN, v.LPlate, n.NID, n.Date AS NoticeDate, n.Time AS NoticeTime, n.Location AS NoticeLocation, i.PID, vc.VioCode, vc.Description AS ViolationDescription, nv.Notes as ViolationNotes, ac.ActCode, ac.Description AS ActionRequired
 	From Person p
     LEFT JOIN Commits c ON p.DLNum = c.DLNum
     LEFT JOIN  Notice n on c.NID = n.NID
+    LEFT JOIN Issues i on n.NID = i.NID
     LEFT JOIN NoticeVio nv on n.NID = nv.NID
     LEFT JOIN ViolationCode vc on nv.VioCode = vc.VioCode
     LEFT JOIN NoticeAction na on n.NID = na.NID
